27c7ca33b0c87040db81f32af384f398d5e88bb1
jaydenpiao <jaydenpiao@gmail.com>
2024-01-08 15:08:33 -0800

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,12 +10,19 @@ const Hero = () => {
         <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
             <h1>UBC's only Poker Club</h1>
             <h2 className='py-4'>Come play!</h2>
-            <a href="https://forms.gle/1dcFGTXf7fTUJLhc9" 
+            <a href="https://docs.google.com/forms/d/1SOqWed70g73BT6xyMY-K4Vt2eg1QiaJwSFnV4xaOZtk/edit" 
               target="_blank" 
               rel="noopener noreferrer" 
               className='p-3 border bg-gradient-to-r from-[var(--primary-dark)] to-[var(--primary-light)] text-white rounded-md inline-block mx-auto font-semibold hover:from-[var(--primary-light)] hover:to-[var(--primary-dark)] transition duration-300'
             >
               Tournament Registration
+            </a>
+            <a href="https://forms.gle/wUfjeCi8zX6Yz9YX9" 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              className='mt-4 p-3 border bg-gradient-to-r from-[var(--primary-dark)] to-[var(--primary-light)] text-white rounded-md inline-block mx-auto font-semibold hover:from-[var(--primary-light)] hover:to-[var(--primary-dark)] transition duration-300'
+            >
+              Winter Festival General Admittance Ticket
             </a>
         </div>
     </div>
--- src/components/Inquiries.jsx
+++ src/components/Inquiries.jsx
@@ -2,68 +2,20 @@ import React, { useState } from 'react'
 import emailjs from '@emailjs/browser'
 
 const Inquiries = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        // emailjs service id, template id, and public key
-        const serviceId = 'service_jx00quc';
-        const templateId = 'template_kxlpb5o';
-        const publicKey = 'EOqH41-yiSffiPKvP';
-
-        // create object
-        const templateParams = {
-            from_name: name,
-            from_email: email,
-            to_name: 'UBC Poker Club',
-            message: message,
-        };
-
-        // send email
-        emailjs.send(serviceId, templateId, templateParams, publicKey)
-            .then((response) => {
-                console.log('Email sent successfully!', response);
-                setName('');
-                setEmail('');
-                setMessage('');
-            })
-            .catch((error) => {
-                console.error('Error sending email:', error);
-            });
-    }
+    
 
   return (
-    <div className='max-w-[1240px] mx-auto px-4 text-center'>
-        <h1>Inquiries</h1>
-        <form onSubmit={handleSubmit} className='flex flex-col items-center justify-center py-4'>
-        <input 
-            type="text"
-            placeholder="Your Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className='mb-4 p-2 border border-gray-300 rounded'
-        />
-        <input 
-            type="email"
-            placeholder="Your Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className='mb-4 p-2 border border-gray-300 rounded'
-        />
-        <textarea
-            cols="30"
-            rows="10"
-            placeholder="Enter text..."
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            className='mb-4 p-2 border border-gray-300 rounded w-full sm:w-[500px] md:w-[600px] lg:w-[700px] h-[200px] sm:h-[250px] font-[Poppins]'
-        >
-        </textarea>
-        <button className='mx-auto font-semibold hover:from-[var(--primary-light)] hover:to-[var(--primary-dark)] transition duration-300'>Send Email</button>
-    </form>
+    <div className='w-full h-screen flex justify-center items-center p-4'>
+        <form method='POST' action='https://getform.io/f/c197ddf8-6806-49b6-92c2-bb772b3272c2' className='flex flex-col max-w-[600px] w-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold text-center'>Inquiries</p>
+            </div>
+            <input className='bg-[#ccd6f6] p-2 rounded-md' type='text' placeholder='Name' name='name' />
+            <input className='my-4 p-2 bg-[#ccd6f6] rounded-md' type='email' placeholder='Email' name='email' />
+            <textarea className='bg-[#ccd6f6] p-2 rounded-md' name='message' rows='10' placeholder='Message'></textarea>
+            <button className='text-white border-2 rounded-md bg-gradient-to-r from-[var(--primary-dark)] to-[var(--primary-light)] px-4 py-3 my-8 mx-auto flex items-center transition ease-in-out font-semibold hover:from-[var(--primary-light)] hover:to-[var(--primary-dark)] duration-300'>Submit</button>
+        </form>
+
     </div>
   )
 }
